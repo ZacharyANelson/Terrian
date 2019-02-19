70753abab33c2add15fa91ddc445c246012a1183
ZacharyANelson <47261023+ZacharyANelson@users.noreply.github.com>
2019-02-18 22:08:24 -0600

    Initial commit

---
+++ Assets/Gun.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 1000f;
+    public Camera Cam;
+    public GameObject impactpart;
+    //public float despawn = 3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetButtonDown("Fire1"))
+        { 
+            Shots();
+        }
+    }
+    void Shots()
+    {
+        RaycastHit hits;
+       if( Physics.Raycast(Cam.transform.position, Cam.transform.forward, out hits, range))
+        {
+            Debug.Log(hits.transform.name);
+            Target target = hits.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TDamage(damage);
+            }
+            Instantiate(impactpart, hits.point, Quaternion.LookRotation(hits.normal));
+            //Destroy(impactpart, despawn);
+        }
+    }
+}
---
+++ Assets/Target.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+
+    public void TDamage ( float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Ded();
+        }
+
+    }
+    void Ded ()
+    {
+        Destroy(gameObject);
+    }
+
+}
