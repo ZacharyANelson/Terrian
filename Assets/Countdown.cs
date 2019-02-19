11d153c77887c07f37e19f0994dac35e17c94b72
ZacharyANelson <47261023+ZacharyANelson@users.noreply.github.com>
2019-02-19 00:11:34 -0600

    added counter
    just added small things

---
+++ Assets/Countdown.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+
+    
+{
+    public float currentTime = 0f;
+    public float startTime = 50f;
+    [SerializeField] Text countdownText;
+    // Start is called before the first frame update
+    void Start()
+
+    {
+        currentTime = startTime;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    currentTime -= 1 * Time.deltaTime;
+        countdownText.text= currentTime.ToString();
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+
+    }
+}
